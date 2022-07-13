94c2e76f6efcf0b6ff3c6120b48ac50e9f8b0c1c
Shashidhar Chavan <105739411+shashidharchavan@users.noreply.github.com>
2022-07-13 14:35:25 +0530

    Add files via upload

---
+++ power_of_a_no.py
@@ -0,0 +1,13 @@
+n=int(input("Enter a num:"))
+p=int(input("Enter power:"))
+
+print(n**p)
+
+
+#using pow()
+
+n1=int(input("Enter a num:"))
+p1=int(input("Enter power:"))
+
+res=pow(n1,p1)
+print(res)
